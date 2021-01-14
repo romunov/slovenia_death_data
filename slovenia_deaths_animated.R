90a6b22fe6fdfb973daacb2a200a909a4a4cfee7
Roman Luštrik <roman.lustrik@biolitika.si>
2021-01-14 09:10:46 +0100

    Update drawing of animated death data for 2016-2020

--- slovenia_deaths_animated.R
+++ slovenia_deaths_animated.R
@@ -1,25 +1,18 @@
 library(ggplot2)
 library(gganimate)
-library(readxl)
 
-# Daily mortality data from Ministry of interior.
-xy <- read_excel("./data/umrljivost_update_2020_11_30.xls", sheet = "adapted")
-xy <- as.data.frame(xy)  # tibble, no thanks
-xy$datum <- as.Date(xy$datum)
-xy <- xy[xy$datum < as.Date("2020-01-01"), ]
-
-# TEMPORARILY(?) because the last update is for 2020 only
-xy2 <- read_excel("./data/umrljivost_updated_2020_12_09.xls", sheet = "adapted")
-xy2 <- as.data.frame(xy2)
-xy2$datum <- as.Date(xy2$datum)
-xy2 <- xy2[xy2$datum < as.Date("2020-12-01"), ]
-
-xy <- rbind(xy, xy2)
-
-xy$value <- as.numeric(xy$value)
+# # Daily mortality data from Ministry of interior for various spatial units and age groups.
+xy <- read.table("./data/mnz_daily_deaths_2021_01_13.csv", header = TRUE, sep = ",")
+xy <- xy[, c("Datum.smrti", "Starostna.skupina.COUNT")]
 colnames(xy) <- c("datum", "count")
+
+xy <- aggregate(count ~ datum, FUN = sum, data = xy)
+
+xy$datum <- as.Date(xy$datum, format = "%d/%m/%Y")
 xy$fake_datum <- as.Date(strftime(xy$datum, format = "0-%m-%d"))
 xy$year <- strftime(xy$datum, format = "%Y")
+
+xy <- xy[order(xy$datum), ]
 
 # Variables needed for the animation.
 xy$frame <- 1:nrow(xy)
@@ -30,9 +23,12 @@ xy$alpha <- 0.5
 xy[xy$year == 2020, "color"] <- "#F4A460"  # sandybrown
 xy[xy$year == 2020, "alpha"] <- 1
 
+# Create a smoothed count which will be used to draw label near the point.
+xy$smoothed <- predict(loess(count ~ as.numeric(datum), data = xy), newdata = xy[, c("datum")])
+
 # > str(xy)
 # 'data.frame':	3984 obs. of  8 variables:
-#   $ datum     : Date, format: "2010-01-01" "2010-01-02" "2010-01-03" ...
+# $ datum     : Date, format: "2010-01-01" "2010-01-02" "2010-01-03" ...
 # $ count     : num  58 43 58 65 49 57 56 53 51 49 ...
 # $ fake_datum: Date, format: "1-01-01" "1-01-02" "1-01-03" ...
 # $ year      : chr  "2010" "2010" "2010" "2010" ...
@@ -40,11 +36,9 @@ xy[xy$year == 2020, "alpha"] <- 1
 # $ color     : chr  "grey" "grey" "grey" "grey" ...
 # $ alpha     : num  0.15 0.15 0.15 0.15 0.15 0.15 0.15 0.15 0.15 0.15 ...
 # $ smoothed  : num  54 54 54 54 54 ...
-
-# Create a smoothed count which will be used to draw label near the point.
-xy$smoothed <- predict(loess(count ~ as.numeric(datum), data = xy), newdata = xy[, c("datum")])
-
+# 
 xy <- xy[xy$datum >= as.Date("2016-01-01"), ]
+xy <- xy[xy$datum <= as.Date("2020-12-31"), ]
 
 minmax <- strftime(xy$datum, format = "%Y")
 minmax <- as.numeric(minmax)
@@ -60,7 +54,7 @@ fig <- ggplot(xy, aes(x = fake_datum, y = count)) +
   caption = "Source: Ministry of interior") +
   geom_line(aes(group = year, color = I(color), alpha = alpha), size = 0.25) +
   geom_point(color = "gray20") +
-  scale_y_continuous(limits = c(0, 125)) +
+  scale_y_continuous(limits = c(0, 135)) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b", expand = expansion(add = c(0, -15))) +
   geom_text(aes(y = smoothed, label = year), nudge_x = 15, size = 3, color = "gray20") +
   coord_polar() +
